af71876e7e73db39be974710101ca66c9479efb4
charles@new-web-sites.com <charles@new-web-sites.com>
2021-07-11 22:29:18 +0100

    update requirements

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'uzm4bf58zejkw(9=!)+gx_&b3f57_040z$&e%^7^b##z^14mc^'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
